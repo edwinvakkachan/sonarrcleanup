74d0fd80f8b6d9fc72b25c33fcd9ab18a2713410
edwin vakkachan <edwinvakkachan@gmail.com>
2026-02-14 12:03:49 +0530

    homeasssitant webhook

--- server.js
+++ server.js
@@ -230,6 +230,32 @@ await fileDelete(queueId);
 
 }
 
+
+async function triggerHAWebhook(errorMessage) {
+  try {
+    await axios.post(
+      "https://homeassistant.wreath.blog:8123/api/webhook/-tcPL5m6CJgyWsnkf8HnZfisV",
+      {
+        status: "error",
+        message: errorMessage,
+        time: new Date().toISOString()
+      },
+      {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        timeout: 5000
+      }
+    );
+
+    console.log("🏠 Home Assistant sonarr webhook triggered");
+    await sendTelegramMessage("🏠 Home Assistant sonarr webhook triggered")
+  } catch (err) {
+    console.error("⚠️ Failed to trigger HA webhook:", err.message);
+    await sendTelegramMessage("⚠️ Failed to trigger HA sonarr webhook")
+  }
+}
+
 async function main() {
   try {
     console.log("🚀 sonarr cleanup started");
@@ -247,8 +273,9 @@ async function main() {
    await sendTelegramMessage("🏁 Cleanup completed successfully")
     process.exit(0); // ✅ clean exit
   } catch (err) {
-    console.error("❌ Cleanup failed:", err.message);
-   await sendTelegramMessage("❌ Cleanup failed:", err.message)
+    console.error("❌ Cleanup failed: triggering HA webhook", err.message);
+   await sendTelegramMessage("❌ Cleanup failed: triggering HA webhook")
+   await triggerHAWebhook('worked')
     process.exit(1); // ❌ failure exit
   }
 }
