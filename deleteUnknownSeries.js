9f262c6eb0c835dffd90957b826f4576389c2600
edwin vakkachan <edwinvakkachan@gmail.com>
2026-02-26 09:40:08 +0530

    igonred tamil and hindi movies

--- deleteUnknownSeries.js
+++ deleteUnknownSeries.js
@@ -47,6 +47,12 @@ const titleName = value.title.toLowerCase();
 if (/\bmalayalam\b/.test(titleName) || /\bmal\b/.test(titleName)) {
   continue;
 }
+if (/\bhindi\b/.test(titleName) || /\bhin\b/.test(titleName)) {
+  continue;
+}
+if (/\btamil\b/.test(titleName) || /\btam\b/.test(titleName)) {
+  continue;
+}
             console.log(`🗑️ ${value.title}`) 
             await sendTelegramMessage(`🗑️ ${value.title}`)
             queueId.push(value.id);
