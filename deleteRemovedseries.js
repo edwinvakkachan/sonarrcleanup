1e17e8383333a37b8520c49947b57a7753b6190b
edwin vakkachan <edwinvakkachan@gmail.com>
2026-02-26 14:18:58 +0530

    fixing dilter

--- deleteRemovedseries.js
+++ deleteRemovedseries.js
@@ -52,6 +52,20 @@ async function getepisodeDetails(epsodeid){
 const queueId =[];
     for (const value of responce.data){
         if(value.trackedDownloadState=='importBlocked'){
+          
+const titleName = value.title.toLowerCase();
+
+if (/\bmalayalam\b/.test(titleName) || /\bmal\b/.test(titleName)) {
+  continue;
+}
+if (/\bhindi\b/.test(titleName) || /\bhin\b/.test(titleName)) {
+  continue;
+}
+if (/\btamil\b/.test(titleName) || /\btam\b/.test(titleName)) {
+  continue;
+}
+
+
             console.log(value.title)
             await sendTelegramMessage(value.title);
             queueId.push(value.id);
