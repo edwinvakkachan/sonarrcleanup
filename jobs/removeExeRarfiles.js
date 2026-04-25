3fba20efcd28a82d31fbc7cc85913cdae73acaa2
edwin vakkachan <edwinvakkachan@gmail.com>
2026-04-26 01:54:41 +0530

    update:skping file redownload in exe file

--- jobs/removeExeRarfiles.js
+++ jobs/removeExeRarfiles.js
@@ -1,9 +1,28 @@
 import axios from "axios";
 import config from "../config.js";
 import { publishMessage } from "../services/publishMessage.js";
-import { fileDelete } from "./fileDelete.js";
 import { delay } from "../utils/delay.js";
 
+async function fileDelete(queueId){
+   await axios.delete(`${config.ip}/api/v3/queue/bulk`,{
+    headers: {
+        "X-Api-Key": config.api
+      },
+      params:{
+        removeFromClient:true,
+        blocklist:true,
+        skipRedownload:true,
+        changeCategory:false
+      },
+      data:{
+        ids:queueId,
+      }
+})
+console.log(`✅ Removed ${queueId.length} Episodes`);
+    await publishMessage({
+  message: `✅ Removed ${queueId.length} Episodes`
+});
+}
 
 export async function removeExeRarfiles(){
 
